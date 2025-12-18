a9c6f22047a26790be5bc7944d789e91d96879f0
Török Péter <183863587+peti9406@users.noreply.github.com>
2025-12-18 14:25:30 +0100

    shadcn/ui installed and delete-modal created

--- frontend/src/contexts/GoalProvider.jsx
+++ frontend/src/contexts/GoalProvider.jsx
@@ -125,7 +125,7 @@ export function GoalProvider({children}) {
             filter,
             filterGoals,
             sorted,
-            sortGoals
+            sortGoals,
         }}>
             {children}
         </GoalContext.Provider>
