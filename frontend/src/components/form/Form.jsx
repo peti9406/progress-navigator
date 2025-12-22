54f19f1b16e099e0ea54d172f70cf3c35b8501d5
Török Péter <183863587+peti9406@users.noreply.github.com>
2025-12-22 10:52:55 +0100

    style(auth): unify login and register page style

--- frontend/src/components/form/Form.jsx
+++ frontend/src/components/form/Form.jsx
@@ -3,7 +3,7 @@ import Button from "../ui/Button.jsx";
 export default function Form({children, onSubmit, header, buttonText}) {
 
     return (
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className='mt-8 px-8 py-4 bg-white/20 border-1 border-white/40 rounded-md shadow-md max-w-max mx-auto'>
             <h1 className="text-3xl">{header}</h1>
             <div className="flex flex-col items-center my-4">
                 {children}
